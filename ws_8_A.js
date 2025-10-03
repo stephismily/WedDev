b38b41c0124ddf0ef557bd26518b42c8583e1cc4
stephismily <stephismily@gmail.com>
2025-10-03 11:29:07 +0530

    JS DOM changes made

---
+++ ws_8_A.js
@@ -0,0 +1,13 @@
+function runCode() {
+    document.getElementById("heading").innerHTML='DOM Lab in Progress.';
+    let intros = document.getElementsByClassName("intro");
+    for(let i=0;i<intros.length;i++){
+        intros[i].style.color = "blue";
+    }
+    let tagPs = document.getElementsByTagName("p");
+    document.querySelector("#output").value=tagPs.length;
+    let notes = document.getElementsByName("note");
+    for(let i=0;i<notes.length;i++){
+        notes[i].style.background = "lightpink";
+    }
+}
