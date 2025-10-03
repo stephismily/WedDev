dc9f6e5a5d93d6e9e83c04a535a99fb216a264c8
STEPHINA SMILY C <stephismily@gmail.com>
2025-10-03 21:42:09 +0530

    JS DOM part F  done

--- ws_8_F.js
+++ ws_8_F.js
@@ -3,8 +3,18 @@ function runCode(){
     let intros = document.querySelectorAll(".intro");
     document.getElementById("output").value = intros.length;
 
-     //Add an input box where a user can type a tag name (e.g., p or li). On pressing a button, display how many such elements are in the document. (Hint: getElementsByTagName).
-    let choice = document.getElementById("input").value;
-    let choices = document.querySelectorAll('choice');
+    //Add an input box where a user can type a tag name (e.g., p or li). On pressing a button, display how many such elements are in the document. (Hint: getElementsByTagName).
+    let choice = document.getElementById("input").value.trim();
+    let choices = document.getElementsByTagName(choice);
     document.querySelector("#output").value = choices.length;
+
+    //Use querySelector("h2") to toggle the heading’s color between red and black every time it’s clicked.
+    let head = document.querySelector("h2");
+    head.onclick = function() {
+        if (head.style.color === "red") {
+            head.style.color = "black";
+        } else {
+            head.style.color = "red";
+        }
+    }
 }
