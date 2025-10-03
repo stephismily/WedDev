f595bd1e856d5c312255045bc48f0e2327391cd4
stephismily <stephismily@gmail.com>
2025-10-03 13:50:34 +0530

    JS DOM part C done

---
+++ ws_8_C.js
@@ -0,0 +1,9 @@
+function runCode(){
+    let paId = document.getElementById("para1");
+
+    let paQ = document.querySelector("#para1");
+    
+    let intros = document.getElementsByClassName("intro");
+
+    let ints = document.querySelectorAll(".intro");
+}
