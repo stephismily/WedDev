5b5968b9ae3f24ececeff48fc67aef57b4b70fa8
stephismily <stephismily@gmail.com>
2025-10-17 12:18:24 +0530

    Book lending app script started

---
+++ Library Book Lending/app.js
@@ -0,0 +1,30 @@
+const appStorage = (function () {
+    const memberKey = 'member';
+    const bookKey = 'book';
+
+    function getMember() {
+        try {JSON.parse(localStorage.getItem(memberKey)); } catch (e) { return null;}
+    }
+
+    function saveMember(userObj) {
+        localStorage.setItem(memberKey, JSON.stringify(userObj));
+    }
+
+    function saveBook(arr) {
+        localStorage.setItem(bookKey, JSON.stringify(bk));
+    }
+
+    function getBook() {
+        try {JSON.parse(localStorage.getItem(bookKey)) || [] ;} catch(e) {return [];}
+    }
+
+    function addBook(bk){
+        const arr = getBook();
+        arr.push(bk);
+        saveBook(arr);
+    }
+
+    return{
+        getMember, saveMember, getBook, saveBook, addBook
+    };
+})();
